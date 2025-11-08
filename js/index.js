683eab20ff08825ef334523d1338c91d1ab5b869
Alberto Kitenge <kitengewalberto@gmail.com>
2025-11-08 08:33:59 +0100

    lesson 6

--- js/index.js
+++ js/index.js
@@ -8,15 +8,50 @@ Alpine.data("signupForm", () => ({
   bio: "",
   newsletter: true,
   showPass: false,
+  errors: {},
 
-  submitForm() {
-    console.log(
-      this.username,
-      this.password,
-      this.belt,
-      this.bio,
-      this.newsletter
-    );
+  validationFrom() {
+    this.errors = {};
+
+    if (this.username.length < 3) {
+      this.errors.username =
+        "Le nom d'utilisateur doit contenir au moins 3 caractères";
+    }
+
+    if (this.password.length < 6) {
+      this.errors.password =
+        "Le mot de passe doit contenir au moins 6 caractères";
+    }
+
+    if (this.password !== this.passwordConfirm) {
+      this.errors.passwordConfirm = "Les mots de passe doivent correspondre";
+    }
+
+    if (!this.belt) {
+      this.errors.belt = "Veuillez sélectionner une couleur de ceinture";
+    }
+
+    if (this.bio.length < 10) {
+      this.errors.bio = "La biographie doit contenir au moins 10 caractères";
+    }
+  },
+
+  submitForm($event) {
+    this.validationFrom();
+
+    console.log(this.errors);
+
+    if (Object.keys(this.errors).length > 0) {
+      console.log(
+        this.username,
+        this.password,
+        this.belt,
+        this.bio,
+        this.newsletter
+      );
+
+      $event.target.reset();
+    }
   },
 }));
 
